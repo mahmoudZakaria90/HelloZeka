ef551797e34d07f83a509ecf37aa36fd190874d3
Mahmoud Zakria <me@zakcoding.com>
2019-02-02 22:34:01 +0100

    change load event to mousemove

--- public/js/main.js
+++ public/js/main.js
@@ -87,14 +87,14 @@ require('./_render.js');
 
 function init(){
 	const message = new SpeechSynthesisUtterance('Good evening, Zakaria what do you seek?');
-	speechSynthesis.speak(message);
+	window.speechSynthesis.speak(message);
 	message.onend = function() {
 		document.querySelector('.intro').className += ' hide';
 		document.body.className = "loaded";
 	}
 }
 
-window.onload = function() {
+window.onmousemove = function() {
 	init();
 }
 
