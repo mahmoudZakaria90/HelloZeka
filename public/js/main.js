138ad0c7fb59a3e494094cc180bceea06cf7527b
Mahmoud Zakria <me@zakcoding.com>
2019-02-05 01:03:40 +0100

    remove audio at all

--- public/js/main.js
+++ public/js/main.js
@@ -77,13 +77,9 @@ module.exports = render;
 //Entry
 require('./_render.js');
 
-const audio = new Audio();
-audio.src = 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/306097/hello.mp3';
 function init(){
-	audio.play();
 	document.querySelector('.intro').className += ' hide';
 	document.body.className = "loaded";
 }
-
-document.addEventListener('mousemove', init, {once: true});
+document.addEventListener('load', init);
 },{"./_render.js":3}]},{},[4]);
--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,8 @@
 //Entry
 require('./_render.js');
 
-const audio = new Audio();
-audio.src = 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/306097/hello.mp3';
 function init(){
-	audio.play();
 	document.querySelector('.intro').className += ' hide';
 	document.body.className = "loaded";
 }
-
-document.addEventListener('mousemove', init, {once: true});
+document.addEventListener('load', init);
